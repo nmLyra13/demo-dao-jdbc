8a2e0fa01bf1d2cacc37e6c1aece6a6f9d0ef7ff
Lyra <nmlyra13@gmail.com>
2025-06-16 20:01:15 -0300

    Creating project and git repository

---
+++ src/model/entities/Department.java
@@ -0,0 +1,36 @@
+package model.entities;
+
+public class Department {
+	
+	private Integer Id;
+	private String name;
+	
+	public Department() {
+		
+	}
+
+	public Department(Integer id, String name) {
+		Id = id;
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+
+}
