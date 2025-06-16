23e1151608e0830b44a523e3d033b42fefcdb004
Lyra <nmlyra13@gmail.com>
2025-06-16 20:25:32 -0300

    Atualização em 16/06/2025 20:25:32

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -1,7 +1,11 @@
 package model.entities;
 
-public class Department {
-	
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable{
+
+	private static final long serialVersionUID = 1L;
 	private Integer Id;
 	private String name;
 	
@@ -28,6 +32,28 @@ public class Department {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		return Objects.equals(Id, other.Id);
+	}
+
+	@Override
+	public String toString() {
+		return "Department [Id=" + Id + ", name=" + name + "]";
 	}
 	
 	
