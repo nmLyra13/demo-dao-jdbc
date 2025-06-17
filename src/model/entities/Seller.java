d83caafd76961459ca7a6fe6caaf1d195ddc46fa
Lyra <nmlyra13@gmail.com>
2025-06-17 15:32:56 -0300

    Update in 17/06/2025 15:32:56

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -1,5 +1,14 @@
 package model.entities;
 
+import java.util.Date;
+
 public class Seller {
 
+	private Integer Id;
+	private String name;
+	private String email;
+	private Date birthDate;
+	private Double baseSalary;
+	
+	private Department department;
 }
