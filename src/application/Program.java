0e26342c0034d55e5ef1c5d13bba7ccf57728e61
Lyra <nmlyra13@gmail.com>
2025-06-19 06:35:29 -0300

    Update in 19/06/2025 06:35:29

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,19 +13,21 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
-		System.out.println("=== TEST 1: seller findById ===");
+
+		System.out.println("=== TEST 1: seller findById =====");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 
-		System.out.println("\n=== TEST 2: seller findByDepartment ===");
+		System.out.println("\n=== TEST 2: seller findByDepartment =====");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
-		for (Seller obj: list) {
+		for (Seller obj : list) {
 			System.out.println(obj);
 		}
 
-	
+		sc.close();
 	}
 }
